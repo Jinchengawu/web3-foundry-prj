da3c156e7f60fdee72fcd3374426783e1896904c
zhanyuan.wang@ximalaya.com <dreamworks.cnn@gmail.com>
2025-07-10 19:56:53 +0800

    feat: 推进bank合约进度

--- src/W1/D3/Bank.sol
+++ src/W1/D3/Bank.sol
@@ -2,30 +2,81 @@ pragma solidity ^0.8.0;
 
 contract Bank {
   //  余额映射，用来存储用户跟存款
-    mapping(address => uint256) public balances;
-    address[3] public Top3Address 
+    mapping(address => uint256) private balances;
+    address[3] private Top3Address;
+    address private owner;
+    address private contractAddress;
 
-    function deposit(uint256 amount) public {
-      balances[msg.sender] = balances[msg.sender] += amount || amount;
-
-
-
+    constructor() {
+      owner = address(this);
+      contractAddress = address(this);
     }
 
+    // 提现
+    function withdraw(address _fromAddress, address _toAddress, uint256 amount) public payable{
+      require(msg.sender == owner, "Only owner can withdraw");
+      require(_toAddress != address(0), "toAddress is not 0");
+      require(_fromAddress != address(0), "_fromAddress is not 0");
+      require(contractAddress.balance >= amount, "Insufficient balance");
+      balances[_fromAddress] -= amount;
+      payable(_toAddress).transfer(amount);
+      getTop3Address();
+    }
+
+//     receive(uint256 amount) external payable{
+// require(msg.value >=0 , "deposit value mast > 0" );     
+//       balances[msg.sender] = balances[msg.sender] > 0 ?  balances[msg.sender] += amount : amount;
+//       if(balances[msg.sender] > balances[Top3Address[0]]){
+//         Top3Address[0] = msg.sender;
+//       }
+//       if(balances[msg.sender] > balances[Top3Address[1]]){
+//         Top3Address[1] = msg.sender;
+//       }
+//       payable(owner).transfer(amount);
+//       getTop3Address();
+//     }
+
+    // 存款
+    function deposit() public payable{
+      require(msg.value >0 , "deposit value mast > 0" );     
+      balances[msg.sender] = balances[msg.sender] += msg.value;
+      if(balances[msg.sender] > balances[Top3Address[0]]){
+        Top3Address[0] = msg.sender;
+      }
+      if(balances[msg.sender] > balances[Top3Address[1]]){
+        Top3Address[1] = msg.sender;
+      }
+    }
+
+    // 获取前3名存款地址
     function getTop3Address() public view returns (address[3] memory) {
 
 
       return Top3Address;
     }
 
-    function withdraw(uint256 amount) public {
-        require(balances[msg.sender] >= amount, "Insufficient balance");
-        balances[msg.sender] -= amount;
+    // 获取合约余额
+    function getBalance() public view returns (uint256) {
+        return owner.balance;
     }
 
-    function getBalance(address account) public view returns (uint256) {
-        return balances[account];
+    // 获取onwer
+    function getOwner() public view returns (address) {
+        return owner;
     }
 
+     // 获取onwer.balance
+    function getOwnerBalance() public view returns (uint256) {
+        return contractAddress.balance;
+    }
 
+    // 获取msg
+    function getMsgAddress() public view returns (address) {
+        return msg.sender;
+    }
+
+    function changeOwner() public {
+      // require(msg.sender == owner, "Only owner can change owner");
+      owner = msg.sender;
+    }
 }
---
+++ test/W1/D3/Bank.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Bank} from "../../../src/W1/D3/Bank.sol";
+
+contract Bank_Test is Test {
+    Bank public bank = new Bank();
+    function getMsgAddress()public view returns (address) {
+        return msg.sender;
+    }
+function test_getMsgAddress()public view returns (address) {
+        return bank.getMsgAddress();
+    }
+   function test_getOwner() public view{
+    assertEq(bank.getOwner(), address(bank));
+   }
+
+   function test_getBalance() public view{
+    assertEq(bank.getBalance(), 0);
+   }
+   
+   function test_deposit() public payable{
+    bank.deposit{value: 1 ether}();
+    assertEq(bank.getBalance(), 1 ether);
+   }
+
+   function test_withdraw() public payable{
+    bank.changeOwner();
+    bank.deposit{value: 1 ether}();
+    assertEq(bank.getBalance(), 1 ether);
+    bank.withdraw(address(this),address(this), 0.1 ether);
+    assertEq(bank.getBalance(), 0);
+   }
+
+// function test_getTop3Address() public payable{
+//     bank.getTop3Address();
+//     assertEq(bank.getTop3Address(), []);
+//    }
+
+}
